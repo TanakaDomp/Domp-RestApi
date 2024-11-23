b542947c4055b8757e6eba9c6031a6e856220d1d
Tanaka Sensei <tanakasennstore@gmail.com>
2024-11-24 03:25:57 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,13 +1,13 @@
-import Link from 'next/link';
-import styles from './app/style.css'; // Pastikan untuk membuat file CSS ini
+// app/page.js
+// https://nextjs.org/docs/app/getting-started/installation
+
+import Link from "next/link"
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>Hellooo Welcome!!!</h1>
-      <Link href="/docs">
-        <a className={styles.link}>Try Ke Docs Page!</a>
-      </Link>
+    <div>
+      <h1>Welcome To My Rest Api!!!</h1>
+      <Link href="/docs">Try the api docs!</Link>
     </div>
-  );
+  )
 }
