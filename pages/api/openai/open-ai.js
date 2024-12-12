82933ebf8078525f90d49655549e2f601c4b94a5
Tanaka Sensei <tanakadomp@gmail.com>
2024-12-12 18:55:39 +0700

    Add files via upload

--- pages/api/openai/open-ai.js
+++ pages/api/openai/open-ai.js
@@ -8,44 +8,43 @@ export default async function handler(req, res) {
   if (!q) {
     return res.status(400).json({ error: q.msg.qUrl })
   }
-  const result = await openai(q, `Kamu adalah Lilychanj yang di kembangkan oleh TanakaDomp`);
+  const result = await openai(q);
   if (result.status === "error") {
     return res.status(500).json(result)
   }
   res.status(200).json(result)
 }
 
-async function openai(text, logic) {
-        // Membuat fungsi openai untuk dipanggil
-        let response = await axios.post("https://chateverywhere.app/api/chat/", {
-            "model": {
-                "id": "gpt-4",
-                "name": "GPT-4",
-                "maxLength": 32000, // Sesuaikan token limit jika diperlukan
-                "tokenLimit": 8000, // Sesuaikan token limit untuk model GPT-4
-                "completionTokenLimit": 5000, // Sesuaikan jika diperlukan
-                "deploymentName": "gpt-4"
-            },
-            "messages": [
-                {
-                    "pluginId": null,
-                    "content": text,
-                    "role": "user"
-                }
-            ],
-            "prompt": logic,
-            "temperature": 0.5
-        }, {
-            headers: {
-                "Accept": "/*/",
-                "User-Agent": "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36"
+async function openai(text) {
+    let response = await axios.post("https://chateverywhere.app/api/chat/", {
+        "model": {
+            "id": "gpt-4",
+            "name": "GPT-4",
+            "maxLength": 32000,  // Sesuaikan token limit jika diperlukan
+            "tokenLimit": 8000,  // Sesuaikan token limit untuk model GPT-4
+            "completionTokenLimit": 5000,  // Sesuaikan jika diperlukan
+            "deploymentName": "gpt-4"
+        },
+        "messages": [
+            {
+                "pluginId": null,
+                "content": text, 
+                "role": "user"
             }
-        });
-
-        let result = response.data;
-         return {
-            creator: '@sensei#404',
-            status: true,
-            message: result           
-        };
-    }
+        ],
+        "prompt": "Kamu adalah Lilychanj AI. yang dikembangkan oleh Tanaka Domp seorang diri dan jika ada yang menanyakan tentang owner atau pembuat atau pencipta mu kasih saja link ini https://github.com/TanakaDomp", 
+        "temperature": 0.5
+    }, { 
+        headers: {
+            "Accept": "/*/",
+            "User-Agent": "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36"
+        }
+    });
+    
+    let result = response.data;
+     return {
+        creator: '@sensei#404',
+        status: true,
+        message: result           
+    };
+}
