6a192febab2f02ad0584a7ca263652f62fac40f7
Tanaka Sensei <tanakasennstore@gmail.com>
2025-02-21 11:35:14 +0700

    Update tiktokdl.js

--- pages/api/downloader/tiktokdl.js
+++ pages/api/downloader/tiktokdl.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 export default async function handler(req, res) {
   if (req.method !== "GET") {
-    return res.status(405).json({ error: "Method not allowed" })
+    return res.status(405).json({ error: "Method not aallowed" })
   }
   const { url } = req.query
   if (!url) {
